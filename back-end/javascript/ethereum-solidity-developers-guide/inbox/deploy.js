ee1fbde37a0076667350dc6b1e7c16ae351d26ef
Paulo Keller <paulo.vinicius.keller@gmail.com>
2019-07-30 23:11:55 -0300

    project added with bug fixes

---
+++ back-end/javascript/ethereum-solidity-developers-guide/inbox/deploy.js
@@ -0,0 +1,24 @@
+const HDWalletProdvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const { interface, bytecode } = require('./compile');
+
+const provider = new HDWalletProdvider(
+  'head code remember wait grant ill urban spike emerge lecture plunge swap',
+  'https://rinkeby.infura.io/v3/a236891c775d4dc9857ede1fca8b15af'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: ['Hi there!'] })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+
+deploy();
---
+++ back-end/javascript/ethereum-solidity-developers-guide/inbox/test/inbox.test.js
@@ -0,0 +1,40 @@
+const assert = require('assert');
+const ganache = require('ganache-cli');
+const Web3 = require('web3');
+const provider = ganache.provider();
+const web3 = new Web3(provider);
+const { interface, bytecode } = require('../compile');
+
+let accounts;
+let inbox;
+const INITIAL_STRING = 'Hi there!';
+const NEW_STRING = 'new message';
+
+beforeEach(async () => {
+  // get a list of all accounts
+  accounts = await web3.eth.getAccounts();
+
+  inbox = await new web3.eth.Contract(JSON.parse(interface))
+    .deploy({ data: bytecode, arguments: [INITIAL_STRING] })
+    .send({ from: accounts[0], gas: '1000000' });
+
+    inbox.setProvider(provider);
+});
+
+describe('Inbox', () => {
+  it('deploys a contract', () => {
+    assert.ok(inbox.options.address);
+  });
+
+  it('has a default message', async () => {
+    const message = await inbox.methods.message().call();
+
+    assert.equal(message, INITIAL_STRING);
+  });
+
+  it('can change the message', async () => {
+    await inbox.methods.setMessage(NEW_STRING).send({ from: accounts[0] });
+    const message = await inbox.methods.message().call();
+    assert.equal(message, NEW_STRING);
+  })
+});
