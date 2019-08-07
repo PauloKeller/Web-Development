9d0bffd3d5a4f0b0c047229adb2a6a0354e4295d
Paulo Keller <paulo.vinicius.keller@gmail.com>
2019-08-06 22:22:29 -0300

    rest part done

---
+++ back-end/Microservices-With-Spring-Boot-Spring-Cloud/restfull-web-services/src/main/java/com/paulokeller/rest/webservices/restfullwebservices/user/PostRepository.java
@@ -0,0 +1,10 @@
+package com.paulokeller.rest.webservices.restfullwebservices.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PostRepository extends JpaRepository<Post, Integer> {
+
+
+}
--- back-end/Microservices-With-Spring-Boot-Spring-Cloud/restfull-web-services/src/main/java/com/paulokeller/rest/webservices/restfullwebservices/user/User.java
+++ back-end/Microservices-With-Spring-Boot-Spring-Cloud/restfull-web-services/src/main/java/com/paulokeller/rest/webservices/restfullwebservices/user/User.java
@@ -3,12 +3,21 @@ package com.paulokeller.rest.webservices.restfullwebservices.user;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.validation.constraints.Past;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.List;
 
 @ApiModel(description = "All details about the user.")
+@Entity
 public class User {
+
+    @Id
+    @GeneratedValue
     private Integer id;
 
     @Size(min = 2, message = "Name should have at least 2 characters")
@@ -18,6 +27,9 @@ public class User {
     @Past
     @ApiModelProperty(notes = "Birth date should be in the past")
     private Date birthDate;
+
+    @OneToMany(mappedBy = "user")
+    private List<Post> posts;
 
     protected User() {
 
@@ -52,6 +64,14 @@ public class User {
 
     public void setBirthDate(Date birthDate) {
         this.birthDate = birthDate;
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(List<Post> posts) {
+        this.posts = posts;
     }
 
     @Override
