f91855c27b469ea9b4a6bee3b5014713c3ac7744
PauloKeller <paulo.vinicius.keller@gmail.com>
2019-08-14 00:57:22 -0300

    using naming server with ribbon

---
+++ back-end/java/Microservices-With-Spring-Boot-Spring-Cloud/currency-conversion-service/src/main/java/com/paulokeller/microservices/currencyconversionservice/CurrencyConversionServiceApplication.java
@@ -0,0 +1,15 @@
+package com.paulokeller.microservices.currencyconversionservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients("com.paulokeller.microservices.currencyconversionservice")
+@EnableDiscoveryClient
+public class CurrencyConversionServiceApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyConversionServiceApplication.class, args);
+	}
+}
