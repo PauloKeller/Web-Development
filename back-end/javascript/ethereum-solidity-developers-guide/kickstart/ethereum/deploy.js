d4d100c87849d892cfc7633609f36705945974b2
PauloKeller <paulo.vinicius.keller@gmail.com>
2019-09-09 23:49:22 -0300

    tests added to project

---
+++ back-end/javascript/ethereum-solidity-developers-guide/kickstart/ethereum/deploy.js
@@ -0,0 +1,25 @@
+const HDWalletProdvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiledFactory = require('./build/CampaignFactory.json');
+
+const provider = new HDWalletProdvider(
+  'head code remember wait grant ill urban spike emerge lecture plunge swap',
+  'https://rinkeby.infura.io/v3/a236891c775d4dc9857ede1fca8b15af'
+);
+
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+
+  console.log('Attempting to deploy from account: ', accounts[0]);
+
+  const result = await new web3.eth.Contract(JSON.parse(compiledFactory.interface))
+    .deploy({ data: compiledFactory.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+  
+  console.log('contract interface: ', compiledFactory.interface);
+  console.log('Contract deployed to: ', result.options.address);
+};
+
+deploy();
