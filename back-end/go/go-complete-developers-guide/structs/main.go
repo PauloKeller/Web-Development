f326f8b6ffa1722a02040dc2596f1aa60b38d2a5
PauloKeller <paulo.vinicius.keller@gmail.com>
2019-10-19 20:21:06 -0300

    structs

---
+++ back-end/go/go-complete-developers-guide/structs/main.go
@@ -0,0 +1,46 @@
+package main
+
+import "fmt"
+
+type contactInfo struct {
+	email   string
+	zipCode int
+}
+
+type person struct {
+	firstName string
+	lastName  string
+	contactInfo
+}
+
+func main() {
+	jim := person{
+		firstName: "Jim",
+		lastName:  "Party",
+		contactInfo: contactInfo{
+			email:   "jim@gmail.com",
+			zipCode: 94000,
+		},
+	}
+
+	jim.updateName("jimmy")
+	jim.print()
+
+	mySlice := []string{"Hi", "There", "How", "Are", "You"}
+
+	updateSlice(mySlice)
+
+	fmt.Println(mySlice)
+}
+
+func updateSlice(s []string) {
+	s[0] = "Bye"
+}
+
+func (p *person) updateName(newFirstName string) {
+	(*p).firstName = newFirstName
+}
+
+func (p person) print() {
+	fmt.Printf("%+v", p)
+}
