b42c49cbaff55aac2dee4c1a8e1a9506b539f5b8
PauloKeller <paulo.vinicius.keller@gmail.com>
2019-11-19 00:35:01 -0300

    go course

---
+++ back-end/go/go-complete-developers-guide/http/main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+)
+
+func main() {
+	resp, err := http.Get("https://google.com")
+	if err != nil {
+		fmt.Println("Error:", err)
+		os.Exit(1)
+	}
+
+	io.Copy(os.Stdout, resp.Body)
+}
