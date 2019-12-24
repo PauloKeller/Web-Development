59837cc1f6bed9fbda6ccca7ba846a2d90fc48ea
PauloKeller <paulo.vinicius.keller@gmail.com>
2019-12-24 14:40:48 -0300

    start to implement the certs

---
+++ back-end/go/gophercon-microservices/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"crypto/tls"
+	"log"
+	"net/http"
+	"os"
+	"time"
+)
+
+const message = "Hello GopherCon UK 2018!"
+
+var (
+	GcukCertFile    = os.Getenv("GCUK_CERT_FILE")
+	GcukKeyFile     = os.Getenv("GCUK_KEY_FILE")
+	GcukServiceAddr = os.Getenv("GCUK_SERVICE_ADDR")
+)
+
+func main() {
+	mux := http.NewServeMux()
+	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(message))
+	})
+
+	srv := NewServer(mux, GcukServiceAddr)
+
+	err := srv.ListenAndServe()
+	if err != nil {
+		log.Fatalf("server failed to start: %v", err)
+	}
+}
+
+func NewServer(mux *http.ServeMux, serverAddress string) *http.Server {
+	tlsConfig := &tls.Config{
+		PreferServerCipherSuites: true,
+		CurvePreferences: []tls.CurveID{
+			tls.CurveP256,
+			tls.X25519,
+		},
+
+		MinVersion: tls.VersionTLS12,
+		CipherSuites: []uint16{
+			tls.TLS_ECDHE_ECDSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_RSA_WITH_AES_256_GCM_SHA384,
+			tls.TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_RSA_WITH_CHACHA20_POLY1305,
+			tls.TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256,
+			tls.TLS_ECDHE_RSA_WITH_AES_128_GCM_SHA256,
+		},
+	}
+
+	srv := &http.Server{
+		Addr:         serverAddress,
+		ReadTimeout:  5 * time.Second,
+		WriteTimeout: 10 * time.Second,
+		IdleTimeout:  120 * time.Second,
+		TLSConfig:    tlsConfig,
+		Handler:      mux,
+	}
+
+	return srv
+}
