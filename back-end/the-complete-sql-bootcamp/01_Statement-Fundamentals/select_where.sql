4f4c8d3bc3cc14ffd17a8facebf4294dec599b32
PauloKeller <paulo.vinicius.keller@gmail.com>
2020-01-18 16:10:43 -0300

    sql bootcamp

---
+++ back-end/the-complete-sql-bootcamp/01_Statement-Fundamentals/select_where.sql
@@ -0,0 +1,9 @@
+SELECT email FROM customer
+WHERE first_name='Nancy'
+AND last_name='Thomas';
+
+SELECT description FROM film
+WHERE title = 'Outlaw Hanky';
+
+SELECT phone FROM address
+WHERE address = '259 Ipoh Drive';
