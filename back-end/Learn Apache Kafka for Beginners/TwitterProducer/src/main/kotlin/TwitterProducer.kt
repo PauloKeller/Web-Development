909659184a9c2e88ed4441ebef57ff649f958bd6
PauloKeller <paulo.vinicius.keller@gmail.com>
2020-05-19 23:09:10 -0300

    twitter producer

--- back-end/Learn Apache Kafka for Beginners/TwitterProducer/src/main/kotlin/TwitterProducer.kt
+++ back-end/Learn Apache Kafka for Beginners/TwitterProducer/src/main/kotlin/TwitterProducer.kt
@@ -8,6 +8,11 @@ import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint
 import com.twitter.hbc.core.processor.StringDelimitedProcessor
 import com.twitter.hbc.httpclient.auth.Authentication
 import com.twitter.hbc.httpclient.auth.OAuth1
+import org.apache.kafka.clients.producer.KafkaProducer
+import org.apache.kafka.clients.producer.ProducerConfig
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.serialization.StringSerializer
+import java.util.*
 import java.util.concurrent.BlockingQueue
 import java.util.concurrent.LinkedBlockingQueue
 import java.util.concurrent.TimeUnit
@@ -19,12 +24,23 @@ class TwitterProducer {
     private var token: String = "968495699947122688-kPmynC3o6JsfkUuclQA8eFg7snRH1IM"
     private var secret: String = "l9xeyPUM4xRWUK5ww6lbCVVwRvBsyYVgvWro0O4Hxuau4"
 
+    val terms: List<String> = Lists.newArrayList("bitcoin", "usa", "politics", "sporte", "soccer")
+
     fun run() {
         print("Setup")
         /** Set up your blocking queues: Be sure to size these properly based on expected TPS of your stream */
         val msgQueue: BlockingQueue<String> = LinkedBlockingQueue(100000)
         val client = createTwitterClient(msgQueue)
         client.connect()
+
+        val producer = createKafkaProducer()
+
+        Runtime.getRuntime().addShutdownHook(object: Thread()  {
+            override fun run() {
+                client.stop()
+                producer.close()
+            }
+        })
 
         while (!client.isDone) {
             var msg: String? = null
@@ -36,6 +52,7 @@ class TwitterProducer {
             }
             if (msg != null) {
                 print(msg)
+                producer.send(ProducerRecord("twitter_tweets", null, msg))
             }
         }
         print("End of application")
@@ -47,7 +64,7 @@ class TwitterProducer {
         val hosebirdHosts: Hosts = HttpHosts(Constants.STREAM_HOST)
         val hosebirdEndpoint = StatusesFilterEndpoint()
 
-        val terms: List<String> = Lists.newArrayList("bitcoin", "api")
+
         hosebirdEndpoint.trackTerms(terms)
 
         val hosebirdAuth: Authentication = OAuth1(consumerKey, consumerSecret, token, secret)
@@ -60,6 +77,27 @@ class TwitterProducer {
                 .processor(StringDelimitedProcessor(msgQueue))
 
         return builder.build()
+    }
+
+    private fun createKafkaProducer(): KafkaProducer<String, String> {
+        val bootstrapServers = "127.0.0.1:9092"
+        val properties = Properties()
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers)
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java.name)
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer::class.java.name)
+        // create safe producer
+        properties.setProperty(ProducerConfig.ENABLE_IDEMPOTENCE_CONFIG, "true")
+        properties.setProperty(ProducerConfig.ACKS_CONFIG, "all")
+        properties.setProperty(ProducerConfig.RETRIES_CONFIG, Integer.MAX_VALUE.toString())
+        properties.setProperty(ProducerConfig.MAX_IN_FLIGHT_REQUESTS_PER_CONNECTION, "5")
+
+        // high throughput producer (at the expense of a bit of latency and CPU usage)
+        properties.setProperty(ProducerConfig.COMPRESSION_TYPE_CONFIG, "snappy")
+        properties.setProperty(ProducerConfig.LINGER_MS_CONFIG, "20")
+        properties.setProperty(ProducerConfig.BATCH_SIZE_CONFIG, (32 * 1024).toString())
+
+        // create the producer
+        return KafkaProducer(properties)
     }
 
 }
