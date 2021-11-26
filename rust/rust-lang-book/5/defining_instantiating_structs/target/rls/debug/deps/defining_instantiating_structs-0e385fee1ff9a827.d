0620b1a9ccab3c675cc3febc8711612f6ece32cf
PauloKeller <paulo.vinicius.keller@gmail.com>
2021-11-26 20:43:46 -0300

    structs

---
+++ rust/rust-lang-book/5/defining_instantiating_structs/target/rls/debug/deps/defining_instantiating_structs-0e385fee1ff9a827.d
@@ -0,0 +1,5 @@
+/home/paulo/Workspace/Studying/web-development/rust/rust-lang-book/5/defining_instantiating_structs/target/rls/debug/deps/defining_instantiating_structs-0e385fee1ff9a827.rmeta: src/main.rs
+
+/home/paulo/Workspace/Studying/web-development/rust/rust-lang-book/5/defining_instantiating_structs/target/rls/debug/deps/defining_instantiating_structs-0e385fee1ff9a827.d: src/main.rs
+
+src/main.rs:
---
+++ rust/rust-lang-book/5/defining_instantiating_structs/target/rls/debug/deps/defining_instantiating_structs-65477843bc298a5d.d
@@ -0,0 +1,5 @@
+/home/paulo/Workspace/Studying/web-development/rust/rust-lang-book/5/defining_instantiating_structs/target/rls/debug/deps/defining_instantiating_structs-65477843bc298a5d.rmeta: src/main.rs
+
+/home/paulo/Workspace/Studying/web-development/rust/rust-lang-book/5/defining_instantiating_structs/target/rls/debug/deps/defining_instantiating_structs-65477843bc298a5d.d: src/main.rs
+
+src/main.rs:
