5f43804f7464bf4e0a9f3c75d4e4e708144f6f6f
PauloKeller <paulo.vinicius.keller@gmail.com>
2021-11-26 21:05:08 -0300

    done structs

---
+++ rust/rust-lang-book/4/slice_type/target/rls/debug/deps/slice_type-6497c5697e56a72b.d
@@ -0,0 +1,5 @@
+/home/paulo/Workspace/Studying/web-development/rust/rust-lang-book/4/slice_type/target/rls/debug/deps/slice_type-6497c5697e56a72b.rmeta: src/main.rs
+
+/home/paulo/Workspace/Studying/web-development/rust/rust-lang-book/4/slice_type/target/rls/debug/deps/slice_type-6497c5697e56a72b.d: src/main.rs
+
+src/main.rs:
---
+++ rust/rust-lang-book/5/rectangles/src/main.rs
@@ -0,0 +1,62 @@
+#[derive(Debug)]
+struct Rectangle {
+    width: u32,
+    height: u32,
+}
+
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+
+    fn width(&self) -> bool {
+        self.width > 0
+    }
+
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+}
+
+impl Rectangle {
+    fn square(size: u32) -> Rectangle {
+        Rectangle {
+            width: size,
+            height: size,
+        }
+    }
+}
+
+fn main() {
+    let rect1 = Rectangle {
+        width: 30,
+        height: 50,
+    };
+    let rect2 = Rectangle {
+        width: 10,
+        height: 40,
+    };
+    let rect3 = Rectangle {
+        width: 60,
+        height: 45,
+    };
+
+    println!("Can rect1 hold rect2? {}", rect1.can_hold(&rect2));
+    println!("Can rect1 hold rect3? {}", rect1.can_hold(&rect3));
+
+    println!("rect1 is {:?}", rect1);
+
+    if rect1.width() {
+        println!("The rectangle has a nonzero width; it is {}", rect1.width);
+    }
+
+
+    println!(
+        "The area of the rectangle is {} square pixels.",
+        rect1.area()
+    );
+
+    dbg!(&rect1);
+}
+
+
