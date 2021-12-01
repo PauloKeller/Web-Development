a15c2500cc9d2d2b9c3b1151651fc962680901e6
PauloKeller <paulo.vinicius.keller@gmail.com>
2021-11-30 22:35:22 -0300

    new files

--- rust/rust-lang-book/2/guessing_game/target/rls/debug/build/libc-0677f5c1168e2ba0/build_script_build-0677f5c1168e2ba0.d
+++
@@ -1,5 +0,0 @@
-/home/paulo/Workspace/Studying/web-development/rust/rust-lang-book/2/guessing_game/target/rls/debug/build/libc-0677f5c1168e2ba0/build_script_build-0677f5c1168e2ba0: /home/paulo/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.108/build.rs
-
-/home/paulo/Workspace/Studying/web-development/rust/rust-lang-book/2/guessing_game/target/rls/debug/build/libc-0677f5c1168e2ba0/build_script_build-0677f5c1168e2ba0.d: /home/paulo/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.108/build.rs
-
-/home/paulo/.cargo/registry/src/github.com-1ecc6299db9ec823/libc-0.2.108/build.rs:
